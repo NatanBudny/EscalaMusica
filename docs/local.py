7b49254dc8dcf0742790ee52aca5dfb956ff0c42
Natan Vilasboa <natan.vilasboa@iatec.com>
2026-01-10 14:36:32 -0300

    version 2.1.0 - create a local server to fast validation

---
+++ docs/local.py
@@ -0,0 +1,23 @@
+import http.server
+import socketserver
+import webbrowser
+import threading
+import os
+
+PORT = 8000
+FILE = "index.html"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        pass  # silencia logs chatos
+
+def open_browser():
+    webbrowser.open(f"http://localhost:{PORT}/{FILE}")
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        threading.Timer(1.0, open_browser).start()
+        print(f"Servidor rodando em http://localhost:{PORT}/{FILE}")
+        httpd.serve_forever()
